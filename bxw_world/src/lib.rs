2ee152466c4cea2c661b1ad473dd476ef9a0cfad
Raven Szewczyk <git@eigenraven.me>
2020-06-01 21:27:15 +0100

    Fix unused variable warning

--- bxw_world/src/lib.rs
+++ bxw_world/src/lib.rs
@@ -151,7 +151,7 @@ impl VCache {
             Some(())
         } else {
             if self.uncompressed_chunks.len() >= self.uncompressed_chunks.cap() {
-                let (k,v) = self.uncompressed_chunks.pop_lru().unwrap();
+                let (_k, _v) = self.uncompressed_chunks.pop_lru().unwrap();
             }
             self.uncompressed_chunks.put(cpos, newest.decompress());
             Some(())
