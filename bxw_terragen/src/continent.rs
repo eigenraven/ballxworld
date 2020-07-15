fb28c4745c9ef6c13ddb314c0c88dca869986b1a
Raven Szewczyk <git@eigenraven.me>
2020-07-15 15:29:54 +0100

    Basic moisture simulation

--- bxw_terragen/src/continent.rs
+++ bxw_terragen/src/continent.rs
@@ -79,8 +79,9 @@ pub struct BiomeCentroid {
     pub debug_shade: Vector3<u8>,
     pub on_edge: bool,
     pub height: ValueAndGradient2<f64>,
+    pub temperature: ValueAndGradient2<f64>,
     pub air_moisture: ValueAndGradient2<f64>,
-    pub ground_moisture: ValueAndGradient2<f64>,
+    pub rainfall: ValueAndGradient2<f64>,
 }
 
 #[derive(Copy, Clone)]
@@ -171,18 +172,31 @@ pub fn generate_continent_tile(
         &hasher,
         (tile_position, &mut biome_points, &biome_point_tree),
     );
+    simulate_temperature(
+        settings,
+        &hasher,
+        (tile_position, &mut biome_points, &biome_point_tree),
+    );
+    simulate_air_moisture(
+        settings,
+        &hasher,
+        (tile_position, &mut biome_points, &biome_point_tree),
+    );
 
     // assign debug shades
     {
         for biome in biome_points.iter_mut() {
             let h = biome.height.0;
+            let t8 = (remap_f(biome.temperature.0, -30.0, 50.0, 0.0, 1.0) * 180.0) as u8;
+            let m8 = (remap_f(biome.air_moisture.0, 0.0, 128.0, 0.0, 1.0).max(0.0).min(1.0) * 255.0) as u8;
+            let r8 = (biome.rainfall.0 * 180.0) as u8;
             if h < 0.0 {
                 let hnorm = (h / SEABED).min(1.0);
-                biome.debug_shade = vec3(0, 0, 255 - (hnorm * 255.0) as u8);
+                biome.debug_shade = vec3(0, t8 / 2, 255 - (hnorm * 255.0) as u8);
             } else {
                 let hnorm = (h / TALLESTMOUNT).min(1.0);
                 let h8 = (hnorm * 255.0) as u8;
-                biome.debug_shade = vec3(h8, 220, h8);
+                biome.debug_shade = vec3(t8, 255-h8, r8);
             }
         }
     }
@@ -385,4 +399,159 @@ fn seed_biome_heights(settings: &ContinentGenSettings, hasher: &Hasher, data: Wi
     }
     // update gradients
     update_gradients(biome_points, |b| &mut b.height);
+}
+
+const TEMPERATURE_NOISE_SCALE: f64 = 10.0;
+
+fn simulate_temperature(settings: &ContinentGenSettings, hasher: &Hasher, data: WipBiomeData) {
+    let rngseed = {
+        let mut h = hasher.clone();
+        h.update(b"biometemperatures");
+        let mut x = h.finalize_xof();
+        let mut s = Seed512::default();
+        x.fill(&mut s.0);
+        s
+    };
+    let mut rng = Xoshiro512StarStar::from_seed(rngseed);
+    let noise_seed = rng.next_u64();
+    let temp_noise = SuperSimplex::new(noise_seed);
+    let (_tile_pos, biome_points, _biome_tree) = data;
+    // initial noise distribution
+    for ipoints in (0..biome_points.len()).step_by(8) {
+        let ipoints8 = usize::min(ipoints + 8, biome_points.len());
+        let mut points_x = WideF64::splat(0.0);
+        let mut points_y = WideF64::splat(0.0);
+        for (lane, point) in biome_points[ipoints..ipoints8].iter().enumerate() {
+            points_x = points_x.replace(lane, point.position.x);
+            points_y = points_y.replace(lane, point.position.y);
+        }
+        let points = vec2(points_x, points_y);
+        let npoints =
+            points / WideF64::splat(settings.biome_avg_distance * TEMPERATURE_NOISE_SCALE);
+
+        let points_temperaturesw =
+            remap_v(temp_noise.vnoise2_wide(npoints), -1.0, 1.0, -30.0, 50.0);
+        let mut points_temperatures = [0.0; 8];
+        points_temperaturesw.write_to_slice_unaligned(&mut points_temperatures);
+        for (lane, point) in biome_points[ipoints..ipoints8].iter_mut().enumerate() {
+            let noise_temp = points_temperatures[lane];
+            let mut temp = noise_temp;
+            let h = point.height.0;
+            if h < 0.0 {
+                temp = (4.0 * temp + 20.0) / 5.0;
+            } else if h > LANDBED {
+                let cooling = (h / TALLESTMOUNT).powi(4);
+                temp = (temp + cooling * -30.0) / (0.5 + cooling);
+            }
+            point.temperature.0 = temp;
+        }
+    }
+    // one round of averaging to minimize extremes
+    {
+        let mut new_temps: Vec<f64> = Vec::new();
+        new_temps.resize(biome_points.len(), 0.0);
+        for (i, point) in biome_points.iter().enumerate() {
+            let mut avg = point.temperature.0;
+            let avgn = 1.0 + point.adjacent.len() as f64;
+            for &ineighbor in point.adjacent.iter() {
+                let ot = biome_points[ineighbor].temperature.0;
+                avg += ot;
+            }
+            new_temps[i] = avg / avgn;
+        }
+        for (i, &t) in new_temps.iter().enumerate() {
+            biome_points[i].temperature.0 = t;
+        }
+    }
+    // update gradients
+    update_gradients(biome_points, |b| &mut b.temperature);
+}
+
+const MOISTURE_SIM_EXTRA_STEPS: u32 = 10;
+const MOISTURE_EVAPORATION: f64 = 128.0;
+
+fn simulate_air_moisture(settings: &ContinentGenSettings, _hasher: &Hasher, data: WipBiomeData) {
+    let continent_size = settings.continent_size as f64;
+    let (_tile_pos, biome_points, _biome_tree) = data;
+    let steps = (continent_size * std::f64::consts::SQRT_2 / settings.biome_avg_distance).round()
+        as u32
+        + MOISTURE_SIM_EXTRA_STEPS;
+    let water_sources: Vec<(usize, f64)> = biome_points
+        .iter()
+        .enumerate()
+        .filter_map(|(i, b)| {
+            if b.height.0 < 0.0 {
+                Some((
+                    i,
+                    MOISTURE_EVAPORATION
+                        * remap_f(b.temperature.0, 4.0, 60.0, 0.0, 1.0)
+                            .max(0.0)
+                            .min(1.0),
+                ))
+            } else {
+                None
+            }
+        })
+        .collect();
+    // (value=moisture, gradient=wind speed)
+    let mut new_moisture: Vec<ValueAndGradient2<f64>> = Vec::new();
+    new_moisture.resize(biome_points.len(), (0.0, vec2(0.0, 0.0)));
+    let mut new_rainfall = Vec::new();
+    new_rainfall.resize(biome_points.len(), 0.0);
+    // initial wind
+    biome_points.iter().enumerate().for_each(|(i, b)| {
+        new_moisture[i].1 = b
+            .temperature
+            .1
+            .try_normalize(1.0e-6)
+            .unwrap_or(vec2(0.0, 0.0))
+    });
+    let stepsf = steps as f64;
+    // simulation
+    for _simstep in 0..steps {
+        // evaporation: add moisture to the air from water sources
+        water_sources.iter().for_each(|&(i, moist)| {
+            new_moisture[i].0 += moist;
+            biome_points[i].air_moisture.0 += moist;
+        });
+        // wind&rainfall simulation
+        for (i, b) in biome_points.iter().enumerate() {
+            let rainfall_factor = if b.height.0 < 0.0 {
+                1.0/(3.0 * stepsf)
+            } else {
+                1.0 - remap_f(b.temperature.0, -40.0, 30.0, 0.1, 0.9)
+                    .max(0.0)
+                    .min(0.8)
+            };
+            new_rainfall[i] += rainfall_factor * b.air_moisture.0;
+            let total_outflow = (1.0 - rainfall_factor) * b.air_moisture.0;
+            for (nidx, &ni) in b.adjacent.iter().enumerate() {
+                let n = &biome_points[ni];
+                let dpos = n.position - b.position;
+                let dnpos = dpos / b.adjacent_distance[nidx];
+                let wind = b.air_moisture.1;
+                let outflow = total_outflow * (dnpos.dot(&wind) + 1.0/b.adjacent.len() as f64).max(-1.0).min(0.5);
+                if outflow > 0.0 {
+                    new_moisture[ni].0 += outflow;
+                    new_moisture[i].0 -= outflow;
+                    new_moisture[ni].1 = ((wind + n.air_moisture.1) / 2.0).normalize();
+                }
+            }
+        }
+        // value clamping
+        new_moisture
+            .iter_mut()
+            .for_each(|v| v.0 = v.0.max(0.0).min(4.0*MOISTURE_EVAPORATION));
+        // update
+        new_moisture
+            .iter()
+            .enumerate()
+            .for_each(|(i, &m)| biome_points[i].air_moisture = m);
+    }
+    // update rainfall
+    let max_rainfall = new_rainfall.iter().fold(0.1f64, |x, &y| x.max(y));
+    new_rainfall.iter().enumerate().for_each(|(i, &rf)| biome_points[i].rainfall.0 = rf / max_rainfall);
+    // update gradients
+    update_gradients(biome_points, |b| &mut b.air_moisture);
+    update_gradients(biome_points, |b| &mut b.rainfall);
 }
